0201a7c7e45b89940e08c1dca004e844881cf7c2
Ömer Ateş <omerates760@gmail.com>
2019-10-06 15:15:37 +0300

    günün yemeği id ye göre değiştirme yapıldı.

--- LoginApp/baglantiYardimcisi.cs
+++ LoginApp/baglantiYardimcisi.cs
@@ -9,7 +9,7 @@ namespace YemekOnerici
     {
         public static string get()
         {
-            return "Server = MELIHALIKAN22F1\\SQLEXPRESS; Database = odev; Integrated Security = SSPI";
+            return "Server =(LocalDB)\\MSSQLLocalDB; Database = odev; Integrated Security = SSPI";
         }
     }
 }
