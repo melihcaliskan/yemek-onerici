0d870efd99bb8d7dbb26dd22c300c5700c7d9255
emregoc <56219956+emregoc@users.noreply.github.com>
2019-12-08 22:36:19 +0200

    .

---
+++ LoginApp/moderator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace YemekOnerici
+{
+    public partial class moderator : Form
+    {
+        public moderator()
+        {
+            InitializeComponent();
+        }
+    }
+}
